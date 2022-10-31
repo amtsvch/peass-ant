0598c3f4853f52bdbfd16020c4494a52ee1420b4
Anna Matusevich <am10tyxu@studserv.uni-leipzig.de>
2022-10-31 23:33:31 +0100

    add main class from tomcat-analysis

---
+++ src/main/java/de/stro18/peass_ant/Main.java
@@ -0,0 +1,41 @@
+package de.stro18.peass_ant;
+
+import java.util.concurrent.Callable;
+
+import de.dagere.peass.*;
+import de.dagere.peass.measurement.cleaning.CleanStarter;
+import de.dagere.peass.measurement.utils.CreateScriptStarter;
+import de.dagere.peass.reexecutions.FindMissingExecutionStarter;
+import de.dagere.peass.visualization.VisualizeRCAStarter;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+
+@Command(name = "peass", mixinStandardHelpOptions = true, subcommands = {
+        SelectStarter.class,
+        MeasureStarter.class,
+        GetChangesStarter.class,
+        CleanStarter.class,
+        IsChangeStarter.class,
+        SearchCauseStarter.class,
+        CreateScriptStarter.class,
+        VisualizeRCAStarter.class,
+        ContinuousExecutionStarter.class,
+        FindMissingExecutionStarter.class},
+        synopsisSubcommandLabel = "COMMAND")
+public class Main implements Callable<Void> {
+
+    public static void main(final String[] args) {
+        final CommandLine line = new CommandLine(new Main());
+        if (args.length != 0) {
+            System.exit(line.execute(args));
+        } else {
+            line.usage(System.out);
+        }
+    }
+
+    @Override
+    public Void call() throws Exception {
+        return null;
+    }
+}
+
